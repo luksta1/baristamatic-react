996f555b86884d1c91907d1d6180dff616828de0
Luke Pura <lukepura@gmail.com>
2018-05-22 09:31:59 -0500

    cleaned up components

--- src/components/InventoryComp.js
+++ src/components/InventoryComp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Image, Table } from 'semantic-ui-react'
+import { Header, Table } from 'semantic-ui-react'
 
 const InventoryComp = (props) => {
     const invItems = Object.keys(props.inventory).sort();
